db656ca223d1519b021a29c4c69e96fe816d213a
dtkachov <dtkachov.codica@gmail.com>
2021-06-06 18:58:07 +0300

    [Fix] create type orm and user create

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
 
-  const PORT = process.env.PORT || 3000;
+  app.setGlobalPrefix('/api/v1');
+
   await app.listen(PORT);
+
   console.log(`Server start on ${PORT}`);
 }
 bootstrap();
