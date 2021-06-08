35e94469bf5de0e8fe2a8090dc0a5e0d6cbd2d4b
TkachovDmitriy <43381128+TkachovDmitriy@users.noreply.github.com>
2021-06-08 22:32:57 +0300

    [Fix] husky (#4)
    Co-authored-by: dtkachov <dtkachov.codica@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { swaggerConfig } from './config/swagger-config';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
 
   app.setGlobalPrefix('/api/v1');
+  SwaggerModule.setup('/api/v1/api-docs', app, document);
 
   await app.listen(PORT);
 
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,26 @@
 import { Get } from '@nestjs/common';
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UserEntity } from './models/user.entity';
 
 import { User } from './models/user.interface';
 import { UserService } from './user.service';
 
-@Controller('user')
+@ApiTags('User')
+@Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiResponse({ status: 200, type: UserEntity, isArray: true })
   @Get()
   getUsers(): Promise<User[]> {
     return this.userService.getUsers();
   }
 
-  @Post()
+  @ApiOperation({ summary: 'Create user' })
+  @ApiResponse({ status: 200, type: UserEntity })
+  @Post('/create')
   createUser(@Body() user: User): Promise<User> {
     return this.userService.createUser(user);
   }
